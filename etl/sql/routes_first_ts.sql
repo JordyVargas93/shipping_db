05b3c4590f08d5582ae1fc9b6aec3b1f61353b18
Jordy Zachary Vargas Gómez <jordy.vargas@adevinta.com>
2020-11-02 22:04:16 -0600

    AVG route time (SQL)

---
+++ etl/sql/routes_first_ts.sql
@@ -0,0 +1,11 @@
+SELECT
+   order_id,
+   status.registered_at AS first_ts
+FROM
+   shipping_db.shipment_order so
+   JOIN
+      status USING(order_id)
+   JOIN
+      status_catalog sc USING(status_catalog_id)
+WHERE
+   sc.status_catalog_id = 1
